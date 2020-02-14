d39ce18958cf8bface1fa96f5b8159de7355dc65
tdt93 <39204219+tdt93@users.noreply.github.com>
2020-02-14 02:16:17 +0100

    source file

---
+++ Flood It: The Game/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "FLOODIT.H"
+
+using namespace std;
+
+/*									FLOOD IT
+	How to play:
+		+Choose game mode:	Easy(5x5 board);
+							Normal (10x10 board);
+							Hard (20x20 board);
+							Custom (dimensions given by user).
+
+		+Starting with color tile from top-left corner.
+		+Following direction in the instruction box to choose the proper color tile.
+		+To escape game, press ESC while playing.
+*/
+
+int main()
+{
+	FloodIt gameOn;
+	
+	return 0;
+}
